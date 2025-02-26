cea6d1d8c1deba292b6e83041b8a4064a7f28be4
alexc@service <alexcdever@gmail.com>
2025-02-26 18:25:46 +0000

    - 避开系统的网络代理配置

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,31 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import electron from 'vite-plugin-electron';
-import renderer from 'vite-plugin-electron-renderer';
 import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [
     react(),
-    electron([
-      {
-        entry: 'electron/main.js',
-      }
-    ]),
-    renderer(),
   ],
   base: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
   },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   server: {
+    host: '0.0.0.0', // 允许局域网访问
     port: 3001,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:9999',
-        changeOrigin: true
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
-    },
+    }
   },
 });
