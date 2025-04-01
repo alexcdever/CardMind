0c226aec0bb23137b2ed2c8ef7ae59c0d627241a
alexc <alexcdever@gmail.com>
2025-04-01 23:50:25 +0800

    - 安装了新依赖和更新了依赖

--- app/windows/flutter/generated_plugin_registrant.cc
+++ app/windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,15 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <flutter_secure_storage_windows/flutter_secure_storage_windows_plugin.h>
+#include <nsd_windows/nsd_windows_plugin_c_api.h>
 #include <sqlite3_flutter_libs/sqlite3_flutter_libs_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterSecureStorageWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterSecureStorageWindowsPlugin"));
+  NsdWindowsPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("NsdWindowsPluginCApi"));
   Sqlite3FlutterLibsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("Sqlite3FlutterLibsPlugin"));
 }
--- app/windows/flutter/generated_plugins.cmake
+++ app/windows/flutter/generated_plugins.cmake
@@ -3,6 +3,8 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  flutter_secure_storage_windows
+  nsd_windows
   sqlite3_flutter_libs
 )
 
