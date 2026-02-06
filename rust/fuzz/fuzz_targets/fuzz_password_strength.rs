60fcde8929c80c618a9d94a35fb6f64df4cad7b5
Alex Chen <alexcdever@gmail.com>
2026-02-06 12:19:48 +0800

    chore: remove fuzz scaffolding

--- rust/fuzz/fuzz_targets/fuzz_password_strength.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-
-use cardmind_rust::security::password::evaluate_password_strength;
-use libfuzzer_sys::fuzz_target;
-
-fuzz_target!(|data: &str| {
-    let _ = evaluate_password_strength(data);
-});
