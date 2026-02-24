3d812d5ee146108d9ffdb43ff101835b0c16261e
Alex Chen <alexcdever@gmail.com>
2026-02-25 05:29:57 +0800

    feat(flutter): add frb bridge and wrapper

---
+++ lib/rust_api.dart
@@ -0,0 +1,12 @@
+import 'bridge_generated/frb_generated.dart';
+
+class RustApi {
+  RustApi._();
+
+  static Future<RustApi> init() async {
+    await RustLib.init();
+    return RustApi._();
+  }
+
+  RustLibApi get api => RustLib.instance.api;
+}
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,4 @@
+mod frb_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 /// 领域模型与错误类型
 pub mod models;
 /// 存储与缓存层
